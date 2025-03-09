516a78fbd33fb69c8d4023e7d4fe661e4366958a
Nikolai Hrusanov 10a klas <159422252+NikolaiHrusanov@users.noreply.github.com>
2025-03-09 15:16:28 +0200

    Add files via upload

---
+++ XStudentsChipApp/UCDownload.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace XStudentsChipApp
+{
+    partial class UCDownload
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UCDownload));
+            this.label1 = new System.Windows.Forms.Label();
+            this.BTN_Download = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(291, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Инсталиране";
+            // 
+            // BTN_Download
+            // 
+            this.BTN_Download.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(164)))), ((int)(((byte)(164)))));
+            this.BTN_Download.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_Download.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BTN_Download.ForeColor = System.Drawing.Color.White;
+            this.BTN_Download.Image = ((System.Drawing.Image)(resources.GetObject("BTN_Download.Image")));
+            this.BTN_Download.Location = new System.Drawing.Point(288, 392);
+            this.BTN_Download.Name = "BTN_Download";
+            this.BTN_Download.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.BTN_Download.Size = new System.Drawing.Size(230, 52);
+            this.BTN_Download.TabIndex = 11;
+            this.BTN_Download.Text = " Инсталиране";
+            this.BTN_Download.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BTN_Download.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.BTN_Download.UseVisualStyleBackColor = false;
+            // 
+            // UCDownload
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.BTN_Download);
+            this.Controls.Add(this.label1);
+            this.Name = "UCDownload";
+            this.Size = new System.Drawing.Size(843, 553);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BTN_Download;
+    }
+}
--- XStudentsChipApp/UCReading.Designer.cs
+++ XStudentsChipApp/UCReading.Designer.cs
@@ -28,20 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UCReading));
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.BTN_search = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.Location = new System.Drawing.Point(353, 62);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(116, 36);
+            this.label1.Size = new System.Drawing.Size(132, 39);
             this.label1.TabIndex = 0;
             this.label1.Text = "Четене";
             // 
@@ -50,7 +52,7 @@
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBox1.Location = new System.Drawing.Point(437, 196);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(253, 44);
+            this.textBox1.Size = new System.Drawing.Size(355, 44);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
@@ -58,14 +60,14 @@
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBox2.Location = new System.Drawing.Point(437, 271);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(253, 44);
+            this.textBox2.Size = new System.Drawing.Size(355, 44);
             this.textBox2.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(261, 204);
+            this.label2.Location = new System.Drawing.Point(274, 199);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(132, 36);
             this.label2.TabIndex = 2;
@@ -81,10 +83,28 @@
             this.label3.TabIndex = 4;
             this.label3.Text = "Потребителско име";
             // 
+            // BTN_search
+            // 
+            this.BTN_search.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(164)))), ((int)(((byte)(164)))));
+            this.BTN_search.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_search.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BTN_search.ForeColor = System.Drawing.Color.White;
+            this.BTN_search.Image = ((System.Drawing.Image)(resources.GetObject("BTN_search.Image")));
+            this.BTN_search.Location = new System.Drawing.Point(397, 389);
+            this.BTN_search.Name = "BTN_search";
+            this.BTN_search.Size = new System.Drawing.Size(161, 52);
+            this.BTN_search.TabIndex = 10;
+            this.BTN_search.Text = "Търсене";
+            this.BTN_search.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BTN_search.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.BTN_search.UseVisualStyleBackColor = false;
+            // 
             // UCReading
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.BTN_search);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
@@ -104,5 +124,6 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button BTN_search;
     }
 }
